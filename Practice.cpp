d32c001419a6bcc45bf8cf046e5716ac0632ad3c
Joshua Meadows <jmeadows4@mail.csuchico.edu>
2020-04-26 13:36:45 -0700

    fix sortDescending

--- Practice.cpp
+++ Practice.cpp
@@ -13,16 +13,16 @@ void Practice::sortDescending(int & first, int & second, int & third)
     first = second;
     second = temp;
   }
-  if( second < third )
-  {
-    int temp = second;
-    second = third;
-    third = temp;
-  }
   if( first < third )
   {
     int temp = first;
     first = third;
+    third = temp;
+  }
+  if( second < third )
+  {
+    int temp = second;
+    second = third;
     third = temp;
   }
 }
