8a497b85763af130acac17bc5c2cefbea48ff42a
Joshua Meadows <jmeadows4@mail.csuchico.edu>
2020-04-26 13:22:30 -0700

    added unit tests for both functions

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -42,7 +42,18 @@ TEST(PracticeTest, odd_palindrome)
     bool actual = obj.isPalindrome("ababa");
     ASSERT_TRUE(actual);
 }
-
+TEST(PracticeTest, special_character_palindrome)
+{
+	Practice obj;
+	bool actual = obj.isPalindrome("{[&]}");
+	ASSERT_TRUE(actual);
+}
+TEST(PracticeTest, single_character)
+{
+	Practice obj;
+	bool actual = obj.isPalindrome("a");
+	ASSERT_TRUE(actual);
+}
 TEST(PracticeTest, descending)
 {
     Practice obj;
@@ -63,7 +74,37 @@ TEST(PracticeTest, ascending)
     ASSERT_TRUE(a == 3 && b == 2 && c == 1);
 }
 
-TEST(PracticeTest, middle_first)
+TEST(PracticeTest, small_big_mid)
+{
+    Practice obj;
+    int a = 1;
+    int b = 3;
+    int c = 1;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 3 && b == 2 && c == 1);
+}
+
+TEST(PracticeTest, big_small_mid)
+{
+    Practice obj;
+    int a = 3;
+    int b = 1;
+    int c = 2;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 3 && b == 2 && c == 1);
+}
+
+TEST(PracticeTest, mid_small_big)
+{
+    Practice obj;
+    int a = 2;
+    int b = 1;
+    int c = 3;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 3 && b == 2 && c == 1);
+}
+
+TEST(PracticeTest, mid_big_small)
 {
     Practice obj;
     int a = 2;
@@ -72,3 +113,29 @@ TEST(PracticeTest, middle_first)
     obj.sortDescending(a, b, c);
     ASSERT_TRUE(a == 3 && b == 2 && c == 1);
 }
+
+TEST(PracticeTest, all_equal)
+{
+    Practice obj;
+    int a = 2;
+    int b = 2;
+    int c = 2;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 2 && b == 2 && c == 2);
+}
+
+TEST(PracticeTest, two_equal)
+{
+    Practice obj;
+    int a = 3;
+    int b = 2;
+    int c = 2;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 3 && b == 2 && c == 2);
+}
+
+
+
+
+
+
